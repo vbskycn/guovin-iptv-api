1da26f59ee8f96bbfd93bac390d04aced47e9c96
vbskycn <zhoujie218@gmail.com>
2024-06-14 10:17:42 +0800

    up

---
+++ user_config.py
@@ -0,0 +1,66 @@
+source_file = "user_demo.txt"
+final_file = "mylist.txt"
+favorite_list = [
+    "CCTV1",
+    "CCTV2",
+    "CCTV3",
+    "CCTV4",
+    "CCTV5",
+    "CCTV5+",
+    "CCTV6",
+    "CCTV7",
+    "CCTV8",
+    "CCTV9",
+    "CCTV10",
+    "CCTV11",
+    "CCTV12",
+    "CCTV13",
+    "CCTV14",
+    "CCTV15",
+    "CCTV16",
+    "CCTV17",
+    "CCTV4K",
+    "CCTV8k",
+]
+open_online_search = False
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = [".php","rtp","key","token","udp"]
+open_subscribe = True
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://mirror.ghproxy.com/raw.githubusercontent.com/ssili126/IPTV/main/cy.txt",
+    "https://mirror.ghproxy.com/raw.githubusercontent.com/kelvinzhu120/iptv-new/main/diy-iptv.txt",
+    "https://mirror.ghproxy.com/raw.githubusercontent.com/dxawi/0/main/tvlive.txt",
+    "http://home.jundie.top:81/Cat/tv/live.txt",
+    "https://weixine.net/tv/ysclive.txt",
+    "https://raw.githubusercontent.com/tangchunwei/iptv-list/main/index.txt",
+    "https://download.kstore.space/download/2863/live.txt",
+    "http://8.210.232.168/xclive.txt",
+    "http://111.67.196.181/mtvzb.txt",
+    "https://raw.githubusercontent.com/vbskycn/guovin-TV/master/result.txt",
+    "https://raw.githubusercontent.com/vbskycn/guovin-TV/master/mylist.txt",
+    "https://tv.zhoujie218.top/dsyy/mylist.txt",
+   "https://raw.githubusercontent.com/shidahuilang/shuyuan/332e3cba56c4f13f41a854ba16e9284c933e26ae/iptv.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/fmml_ipv6.txt",
+    "https://live.zhoujie218.top/jdtv/itvlist.txt",
+    "https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/ycl_iptv.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/y_g.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/j_home.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/j_iptv.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/o_s_cn.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/o_s_cn_112114.txt",
+    "https://iptv.my.zhoujie218.top:8443/txt/cn.txt",
+]
+open_multicast = True
+region_list = ["all"]
